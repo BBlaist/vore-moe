9b9d40e188bd5eb8e6ffb960b6f4e634d07fc76a
BBlaist <89626@glr.nl>
2024-12-03 21:58:18 +0100

    temporary styling for look and feel

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,23 +1,30 @@
 import Image from "next/image";
 import React from "react";
 import Background from "/public/place_back.jpeg";
+import Logo from "/public/VoreM.png";
 
 export default function Main() {
   return (
     <div className="h-screen relative">
-      <Image
-        src={Background}
-        alt="Background"
-        fill={true}
-      />
-      <div className="absolute w-full h-full">
-      <div className="flex justify-around align-center h-full text-white">
-        <div>content-left</div>
-        <div>content-right</div>
-      </div>
-      <div className="text-white w-full">
-        <a href="#">ABOUT</a> <a href="#">OUR WORK</a> <a href="#">CONTACT</a>
-      </div>
+      <div className="absolute w-full h-full bg-filtered"></div>
+      <div className="absolute w-full h-full flex items-center justify-between px-4">
+        <div className="text-white w-full flex">
+          <div className="ml-4 md:ml-4 lg:ml-80">
+            <Image 
+            src={Logo} 
+            alt="Logo" 
+            className = "w-3/6 h-auto md:w-4/6 lg:w-full"
+            />
+          </div>
+          <div></div>
+        </div>
+        <div className="text-white w-full absolute inset-x-0 bottom-0 flex justify-center gap-10">
+          <a href="#" className="mx-8 sm:mx-4 text-1xl md:text-2xl sm:text-3xl">ABOUT</a>
+          <p className="text-2xl">-</p>
+          <a href="#" className="mx-8 sm:mx-4 text-1xl md:text-2xl sm:text-3xl">OUR WORK</a>
+          <p className="text-2xl">-</p>
+          <a href="#" className="mx-8 sm:mx-4 text-1xl md:text-2xl sm:text-3xl">CONTACT</a>
+        </div>
       </div>
     </div>
   );

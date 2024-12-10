d551f574b0fdd744b36b3c54a3f8e19331985c35
bblaist <dirkrijntalder@gmail.com>
2024-12-10 17:25:17 +0100

    good looks

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Background from "/public/home-bg.png";
 import Logo from "/public/home-skull.png";
 import HomeLogo from "/public/home_logo.png";
+import Insta from "/public/insta.png";
 
 export default function Main() {
   return (
@@ -24,23 +25,34 @@ export default function Main() {
             <Image
             src={HomeLogo}
             alt="Logo"
-            width={500}
-            height={500}
-            className="absolute bottom-48 w-full h-auto"
+            width={450}
+            height={450}
+            className="absolute bottom-64 h-auto"
+            
             />
             <div className="w-full flex justify-center">
-              <div className="absolute bottom-44 w-2/3 font-light text-[20px] [word-spacing:8px] tracking-wide">Cables to bring your build's Aesthetics to the next level.</div>
+              <div className="absolute bottom-[17rem] font-bold text-slate-300 text-[15px] [word-spacing:8px] tracking-wide">CABLES TO BRING YOUR BUILD'S <br></br> AESTHETICS TO THE NEXT LEVEL.</div>
+              <div className="">
+                <a href="https://www.instagram.com/vore.moe/">
+                <Image
+                src={Insta}
+                alt="Instagram"
+                width={50}
+                height={50} 
+                />
+                </a>
+              </div>
             </div>
             
             </div>
           <div></div>
         </div>
-        <div className="text-white w-full absolute inset-x-0 bottom-0 flex justify-center gap-10">
-          <a href="#" className="mx-8 sm:mx-4 text-1xl md:text-2xl sm:text-3xl">ABOUT</a>
+        <div className="text-white w-full absolute inset-x-0 bottom-4 flex justify-center gap-10">
+          <a href="#" className="mx-8 sm:mx-4 text-1xl md:text-xl sm:text-2xl">ABOUT</a>
           <p className="text-2xl">-</p>
-          <a href="#" className="mx-8 sm:mx-4 text-1xl md:text-2xl sm:text-3xl">OUR WORK</a>
+          <a href="#" className="mx-8 sm:mx-4 text-1xl md:text-xl sm:text-2xl">OUR WORK</a>
           <p className="text-2xl">-</p>
-          <a href="#" className="mx-8 sm:mx-4 text-1xl md:text-2xl sm:text-3xl">CONTACT</a>
+          <a href="#" className="mx-8 sm:mx-4 text-1xl md:text-xl sm:text-2xl ">CONTACT</a>
         </div>
       </div>
     </div>
